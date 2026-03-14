5a74f1994b2114b317101aecc8fdfc6ba43613ba
Mexy <ianbrgamer1406@gmail.com>
2026-03-13 23:13:00 -0300

    fix: corrigir leitura de resources e atualizar arquivo .gitignore

--- main/src/repository/FormularioRepository.java
+++ main/src/repository/FormularioRepository.java
@@ -1,46 +1,38 @@
 package repository;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FormularioRepository {
 
     public List<String> lerPerguntas() {
-        Path path = Paths.get("main", "resources", "formulario.txt");
-        try {
-            return Files.readAllLines(path);
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo no caminho: " + path.toAbsolutePath());
-            return new ArrayList<>();
-        }
+        return lerArquivoDoClasshPath("/formulario.txt");
     }
 
 
     public List<String> lerMenu() {
-        Path path = Paths.get("main", "resources", "menu.txt");
-
-        try {
-            return Files.readAllLines(path);
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo no caminho: " + path.toAbsolutePath());
-            return new ArrayList<>();
-        }
+        return lerArquivoDoClasshPath("/menu.txt");
     }
 
     public List<String> lerPerguntasEndereco() {
-        Path path = Paths.get("main", "resources", "formularioEndereco.txt");
-        try {
-            return Files.readAllLines(path);
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo no caminho: " + path.toAbsolutePath());
+       return lerArquivoDoClasshPath("/formularioEndereco.txt");
+    }
+
+    private List<String> lerArquivoDoClasshPath(String nomeArquivo) {
+        try (InputStream is = getClass().getResourceAsStream(nomeArquivo);
+             BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+
+            if (is == null) {
+                System.out.println("Aviso: Arquivo: " + nomeArquivo + " não encontrado no classpath.");
+            }
+
+            return br.lines().toList();
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o arquivo no caminho: " + nomeArquivo);
             return new ArrayList<>();
         }
     }
